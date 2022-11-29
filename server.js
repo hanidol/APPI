5bedbb816212969e996c34df4c340b63aef408bb
hanidol <haninasri14@gmail.com>
2022-11-29 12:23:23 +0100

    make it better 3

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, "/client")));
 
