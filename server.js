e92fc53115133228a97aa878add9e3bc88c6c00b
hanidol <haninasri14@gmail.com>
2022-11-29 12:36:46 +0100

    make it better 3

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ connectDB();
 
 // Initialize Middleware
 app.use(express.json({ strict: false }));
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
