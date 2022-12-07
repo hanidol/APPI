3216a18f53af41757d16fa47ca5db80c991f9f8a
hanidol <haninasri14@gmail.com>
2022-12-07 16:50:16 +0100

    update

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use("/api/posts", require("./routes/api/posts"));
 app.use(cors());
 
 //app.use(express.static(path.join(__dirname, "/client/build")));
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 //app.get("*", (req, res) => {
 //res.sendFile(path.join(__dirname, "/client/build", "index.html"));
