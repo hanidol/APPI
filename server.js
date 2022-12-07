8d7aa78152a0a1232d36e4631eda7fba6b7084c8
hanidol <haninasri14@gmail.com>
2022-12-07 12:22:24 +0100

    update

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(express.static(path.join(__dirname, "./client/build")));
 //res.sendFile(path.join(__dirname, "/client/build", "index.html"));
 //});
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 // SERVER
